1cb1fc7b4e65a075c181e72bf4b35afa0b111c5d
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-02-07 11:45:07 -0600

    created functionality for post button

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// your code here
+var submit = document.getElementById('submit');
+
+submit.addEventListener('click', function () {
+  var userName = document.getElementById('name').value;
+  var userInput = document.getElementById('message').value;
+  var newPost = document.createElement('div');
+  var postAuthor = document.createElement('div');
+  var userNameElement = document.createTextNode('Posted By: ' + userName);
+  var userInputElement = document.createTextNode(userInput);
+
+  newPost.appendChild(userInputElement);
+  postAuthor.appendChild(userNameElement);
+  
+  document.getElementsByClassName('posts')[0].append(newPost);
+  document.getElementsByClassName('posts')[0].append(postAuthor);
+});
